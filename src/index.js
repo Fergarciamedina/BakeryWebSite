fa91665d98d99edc0d547bcdbf6fb61632360f28
Fernando Manuel Garcia Medina <fernando.garcia@utec.edu.pe>
2023-03-19 22:31:05 -0500

    .

--- src/index.js
+++ src/index.js
@@ -13,16 +13,7 @@ import OwlCarousel from 'react-owl-carousel';
 import '../lib/owlcarousel/assets/owl.carousel.css';
 import '../lib/owlcarousel/assets/owl.theme.default.css';
 
-function Spinner() {
-  return (
-    <div
-      id="spinner"
-      className="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center"
-    >
-      <div className="spinner-grow text-primary" role="status"></div>
-    </div>
-  );
-}
+
 
 function TopBar() {
   return (
